e893e82c25100350ab74573837733b9281bf211b
Baldrian Sector <baldriansector@gmail.com>
2025-07-28 12:19:54 +0200

    Minor text tweaks

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,6 +4,7 @@ import Section from "./Section";
 const NotFound = () => {
   return (
     <Section className="bg-dark text-light flex flex-col items-center justify-center text-center">
+      <h1 className="text-8xl font-bold mb-12">404</h1>
       <h2 className="text-2xl font-bold">Something went wrong...</h2>
       <p className="mt-4 italic">
         But every mistake is a learning opportunity, right?
