c76529d2c94598938309a3ef9102defc7b8d051e
Baldrian Sector <baldriansector@gmail.com>
2025-08-01 12:34:54 +0200

    Update contact section

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,18 +1,15 @@
 const ContactSection = () => {
   return (
     <div className="flex flex-col items-start mx-10 pt-10">
-      <img
-        src="/assets/images/Contact Portrait.jpg"
-        alt="Portrait of Baldrian"
-        className="max-w-sm mb-10 shadow-lg max-h-80 w-auto"
-      />
-
       <h2 className="text-4xl font-bold mb-4">@ Get in contact</h2>
 
       <p className="about-text text-lg font-medium mb-4 max-w-xl leading-relaxed text-gray">
         I’m always open to new opportunities and collaborations. Whether you
-        have a specific project in mind or just want to say hello. Currently,
-        I'm searhing for an internship position for the fall of 2025.
+        have a specific project in mind or just want to say hello. Currently,{" "}
+        I'm searching for an{" "}
+        <span className="underline decoration-2">
+          internship position for the fall of 2025.
+        </span>
       </p>
       <p className="about-text text-lg font-medium mb-4 max-w-xl leading-relaxed text-gray">
         You can contact me directly via email at{" "}
