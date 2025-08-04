068f32d783529d3bf757f86b994096f1510cfb54
Baldrian Sector <baldriansector@gmail.com>
2025-08-05 01:22:28 +0200

    Article links open in new tab

--- src/components/TextBlock.jsx
+++ src/components/TextBlock.jsx
@@ -2,7 +2,17 @@ import ReactMarkdown from "react-markdown";
 
 const TextBlock = ({ blok }) => (
   <div className="prose prose-invert article-text text-lg py-12 max-w-2xl mx-auto my-4 text-dark font-normal">
-    <ReactMarkdown>{blok.text}</ReactMarkdown>
+    <ReactMarkdown
+      components={{
+        a: ({ href, children, ...props }) => (
+          <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+            {children}
+          </a>
+        ),
+      }}
+    >
+      {blok.text}
+    </ReactMarkdown>
   </div>
 );
 
