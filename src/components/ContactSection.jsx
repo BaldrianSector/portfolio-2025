6c773167ab770d644c57007a84b77d83f53267fb
Baldrian Sector <baldriansector@gmail.com>
2025-09-16 22:36:31 +0200

    Best commit this year — internship acquired 🎉

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -8,10 +8,10 @@ const ContactSection = () => {
 
         <p className="contact-text text-lg font-medium mb-4 max-w-xl leading-relaxed text-black">
           I’m always open for new opportunities and collaborations, whether you
-          have a specific project in mind or just want to say hello. Right now,{" "}
-          I'm searching for an{" "}
-          <span className="underline decoration-2">
-            internship position for the fall of 2025.
+          have a specific project in mind or just want to say hello.{" "}
+          <span className="line-through decoration-2">
+            Right now, I'm searching for an internship position for the fall of
+            2025.
           </span>
         </p>
         <p className="contact-text text-lg font-medium mb-2 max-w-xl leading-relaxed text-gray italic">
